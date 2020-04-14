f35e553d38cbdbcf54af1e9492f826f0bebf7382
swebfay <swebfay@gmail.com>
2020-04-14 11:09:07 +0100

    new commit

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,15 @@
 $(function(){
 
-var mixer = mixitup('products__inner-box');
+var mixer = mixitup('.products__inner-box');
+
+$(".rate-star").rateYo({
+    rating: 5,
+    starWidth: "12px",
+    readOnly: true
+  });
+
+$(".product-slider__inner").slick({
+  
+});
 
 });
